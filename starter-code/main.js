f140b493682634b3dc83f353ba1cc8cad8849c07
cyberhooli <julianamichelsen@Julis-MacBook-Air.local>
2017-02-26 14:34:51 -0600

    unit 9 exercises, pop ups and corrected statements

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,12 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert ('Sorry, try again.');
+} else if (cardTwo === cardOne) {
+	alert ('You found a match!');
+ } else if (cardThree === cardFour){
+	alert ('You found a match!'); 
+} else if (cardThree === cardTwo){
+	alert ('Sorry, try again.'); 
